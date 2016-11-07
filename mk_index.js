3bdb6f4b1f84bccb44d198a365e305807f299f98
dorisofsky <dorisofsky@gmail.com>
2016-11-07 15:52:52 +0800

    壞掉啦哭哭 快點復原復原

--- mk_index.js
+++ mk_index.js
@@ -13,9 +13,8 @@ var csv = d3.dsv(",","text/csv;charset=big5");
 
 
         data.forEach(function(d){
-            
-            d.Time=timeparse(d.Time);
             d.date=dateformat(timeparse(d.Time)); //新增
+            d.Time=timeparse(d.Time);
         });
 
         var ndx = crossfilter(data);
