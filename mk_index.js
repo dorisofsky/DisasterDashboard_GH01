28125a72021c6a32dd59d53b54f206d491ad1dd1
dorisofsky <dorisofsky@gmail.com>
2016-11-14 11:52:26 +0800

    好像某些順序不能亂換QQ

--- mk_index.js
+++ mk_index.js
@@ -84,8 +84,8 @@ var csv = d3.dsv(",","text/csv;charset=big5");
             .xAxisLabel("Date")
             .centerBar(true)
             .xUnits(function(d){return 62})
-            .xAxis().tickFormat(d3.time.format('%d'))
-            .brushOn(true);
+            .brushOn(true)
+            .xAxis().tickFormat(d3.time.format('%d'));
 
 
         var countyRowChart = dc.rowChart("#chart-row-county")
