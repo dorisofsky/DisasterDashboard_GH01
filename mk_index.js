f668183815c4ec06beb8a462c923374058c5d755
dorisofsky <dorisofsky@gmail.com>
2016-11-14 12:02:24 +0800

    加上.outerPadding(0.05)想要讓第一個小時的label出現

--- mk_index.js
+++ mk_index.js
@@ -83,6 +83,7 @@ var csv = d3.dsv(",","text/csv;charset=big5");
             .x(d3.time.scale().domain([minTime, maxTime]))
             .xAxisLabel("Date")
             .centerBar(true)
+            .outerPadding(0.05)
             .xUnits(function(d){return 62})
             .brushOn(true)
             .xAxis().tickFormat(d3.time.format('%m/%e %H:%M'));
