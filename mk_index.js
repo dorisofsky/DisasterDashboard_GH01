daf8192d86d1f6f05b631e00bdfb4e3c8c656b3c
dorisofsky <dorisofsky@gmail.com>
2016-11-14 10:22:25 +0800

    再稍微調整一下畫布長寬

--- mk_index.js
+++ mk_index.js
@@ -69,9 +69,9 @@ var csv = d3.dsv(",","text/csv;charset=big5");
 
         var timechart =dc.barChart("#dis_time")
             .width(730)
-            .height(180)
+            .height(200)
             .transitionDuration(500)
-            .margins({top: 0, right: 20, bottom: 50, left: 30})
+            .margins({top: 0, right: 20, bottom: 45, left: 30})
             .dimension(hourdim)
             .group(FloodGroup,"Flood")
             .stack(LandslideGroup,"Landslide")
